b080d84b8dbb9e6080422eced20c298d632be441
opeyem1a <opeyadeyemi@gmail.com>
2020-11-12 02:04:25 -0800

    fetch questions and setup object with questions and shuffled* answers

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
-$(function() {
-    
-})
+const csvBaseName = "module";
+
+$(function () {
+  populateQuiz();
+});
+
+const populateQuiz = () => {
+  fetch(`./assets/${csvBaseName}${getModuleNumber()}.csv`)
+    .then((response) => response.text())
+    .then((text) => {
+      let csvData = $.csv.toObjects(text);
+      csvData = csvData
+        .filter((qObj) => qObj.section == getSectionNumber())
+        .map((qObj) => {
+          return {
+            question: qObj.question,
+            options: [
+              { answer: qObj.answer },
+              { d1: qObj.distractor1 },
+              { d2: qObj.distractor2 },
+            ].sort(() => Math.random() - 0.5),
+          };
+        });
+      console.log(csvData);
+    });
+};
+
+const getSectionNumber = () => {
+  return 1;
+};
+
+const getModuleNumber = () => {
+  return 2;
+};
